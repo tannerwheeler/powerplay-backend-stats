287890df1f14990340264c1f256414cf29dfc731
Caleb <caleb.r.edwards@gmail.com>
2024-05-18 09:31:20 -0600

    First attempt to connect to db

--- backend/internal/server/apis/stats/penalty.go
+++ backend/internal/server/apis/stats/penalty.go
@@ -1,12 +1,19 @@
 package stats
 
 import (
-	"encoding/json"
-
 	"github.com/gofiber/fiber/v2"
 	"github.com/jak103/powerplay/internal/server/apis"
 	"github.com/jak103/powerplay/internal/server/services/auth"
+	"github.com/jak103/powerplay/internal/utils/log"
+	"github.com/jak103/powerplay/internal/utils/responder"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
+
+type PenaltyType struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
 
 func init() {
 	apis.RegisterHandler(fiber.MethodGet, "/penaltyTypes", auth.Public, getPenaltyTypes)
@@ -14,18 +21,38 @@ func init() {
 
 func getPenaltyTypes(c *fiber.Ctx) error {
 
-	penaltyTypes := stubPenaltyTypes()
-	jsonData, err := json.Marshal(penaltyTypes)
+	var penaltyType PenaltyType
+
+	dsn := "host=localhost user=postgres password=password dbname=powerplay port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		return err
+		log.Error("Cannot connect to db, %v", err)
 	}
 
-	c.Type("json")
+	result := db.First(&penaltyType)
+	if result.Error != nil && result.Error != gorm.ErrRecordNotFound {
+		log.Warn("Error occurred while querying database")
+		return responder.InternalServerError(c)
+	} else {
+		if penaltyType.ID != 0 || penaltyType.Name != "" {
+			return responder.OkWithData(c, penaltyType)
+		} else {
+			return responder.OkWithData(c, "No Data Found")
+		}
+	}
 
-	// Send JSON response
-	return c.Send(jsonData)
+	// penaltyTypes := stubPenaltyTypes()
+	// jsonData, err := json.Marshal(penaltyTypes)
+	// if err != nil {
+	// 	return err
+	// }
+
+	// c.Type("json")
+
+	// // Send JSON response
+	// return c.Send(jsonData)
 }
 
-func stubPenaltyTypes() map[string][]string {
-	return map[string][]string{"penaltyTypes": []string{"Boarding", "Charging", "Slashing"}}
-}
+// func stubPenaltyTypes() map[string][]string {
+// 	return map[string][]string{"penaltyTypes": {"Boarding", "Charging", "Slashing"}}
+// }
