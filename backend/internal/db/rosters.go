7421725765060a84b60d54657148aaf8bb2a3106
caleb <caleb.r.edwards@gmail.com>
2024-05-28 21:56:25 -0600

    init at adding queries

--- backend/internal/db/rosters.go
+++ backend/internal/db/rosters.go
@@ -2,8 +2,14 @@ package db
 
 import "github.com/jak103/powerplay/internal/models"
 
-func (s session) GetRosters() ([]models.Roster, error) {
+func (s session) GetRosters(query RosterQuery) ([]models.Roster, error) {
 	rosters := make([]models.Roster, 0)
+
+	teamName := query.TeamName
+	if teamName != nil {
+		s.connection.Where("")
+	}
+
 	err := s.connection.Find(&rosters)
 	return resultsOrError(rosters, err)
 }
--- backend/internal/server/apis/rosters/rosters.go
+++ backend/internal/server/apis/rosters/rosters.go
@@ -2,8 +2,11 @@ package roster
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/jak103/powerplay/internal/db"
+	"github.com/jak103/powerplay/internal/models"
 	"github.com/jak103/powerplay/internal/server/apis"
 	"github.com/jak103/powerplay/internal/server/services/auth"
+	"github.com/jak103/powerplay/internal/utils/log"
 	"github.com/jak103/powerplay/internal/utils/responder"
 )
 
@@ -12,8 +15,17 @@ func init() {
 }
 
 func getRosters(c *fiber.Ctx) error {
-	// TODO - look at optional params 
-	// c.ParamsParser()
-	// c.QueryParser()
-	return responder.NotYetImplemented(c)
+
+	query := &models.RosterQuery{}
+
+	c.QueryParser(query)
+
+	db := db.GetSession(c)
+	penaltyTypes, err := db.GetRosters(query)
+	if err != nil {
+		log.WithErr(err).Alert("Failed to get all penalty types from the database")
+		return err
+	}
+
+	return responder.OkWithData(c, penaltyTypes)
 }
