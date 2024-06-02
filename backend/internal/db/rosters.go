bbc5f828186d41700b84db4569b5a384c3dc6738
caleb <caleb.r.edwards@gmail.com>
2024-06-02 00:15:05 -0600

    Roster can post given capt email, but get all not totally working

--- backend/internal/db/rosters.go
+++ backend/internal/db/rosters.go
@@ -1,9 +1,44 @@
 package db
 
-import "github.com/jak103/powerplay/internal/models"
+import (
+	"github.com/jak103/powerplay/internal/models"
+)
+
+func (s session) GetRoster(rosterName string) (*models.Roster, error) {
+	roster := &models.Roster{}
+
+	result := s.connection.Where("roster_name = ?", rosterName).First(roster)
+
+	return resultOrError(roster, result)
+}
 
 func (s session) GetRosters() ([]models.Roster, error) {
 	rosters := make([]models.Roster, 0)
+
 	err := s.connection.Find(&rosters)
+
 	return resultsOrError(rosters, err)
+}
+
+func (s session) PostRoster(roster *models.Roster) error {
+	result := s.connection.Create(roster)
+
+	return result.Error
+}
+
+// func (s session) PostUserToRoster(rosterName string, user models.User) error {
+// 	roster, err := s.GetRoster(rosterName)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	return err
+// }
+
+func (s session) GetUserByEmail(email string) (*models.User, error) {
+	user := &models.User{}
+
+	result := s.connection.Where("email = ?", email).First(user)
+
+	return resultOrError(user, result)
 }
--- backend/internal/server/apis/rosters/rosters.go
+++ backend/internal/server/apis/rosters/rosters.go
@@ -3,6 +3,7 @@ package roster
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/jak103/powerplay/internal/db"
+	"github.com/jak103/powerplay/internal/models"
 	"github.com/jak103/powerplay/internal/server/apis"
 	"github.com/jak103/powerplay/internal/server/services/auth"
 	"github.com/jak103/powerplay/internal/utils/log"
@@ -10,7 +11,57 @@ import (
 )
 
 func init() {
+	apis.RegisterHandler(fiber.MethodPost, "/rosters", auth.Public, postRoster)
 	apis.RegisterHandler(fiber.MethodGet, "/rosters", auth.Public, getRosters)
+	// apis.RegisterHandler(fiber.MethodPost, "/rosters", auth.Public, postRosters)
+}
+
+func postRoster(c *fiber.Ctx) error {
+	db := db.GetSession(c)
+
+	captEmail := c.Query("capt_email")
+	if captEmail == "" {
+		return responder.BadRequest(c, "Empty Captain Email, Pass in valid value")
+	}
+
+	capt, err := db.GetUserByEmail(captEmail)
+	if err != nil {
+		return responder.InternalServerError(c, "Error getting captain in database")
+	}
+
+	if capt == nil {
+		return responder.BadRequest(c, "No captain in database")
+	}
+
+	// rosterName := c.Query("roster_name")
+	// if rosterName == "" {
+	// 	return responder.BadRequest(c, "Empty Roster Name, Pass in valid value")
+	// }
+
+	// email := c.Query("email")
+	// if email == "" {
+	// 	return responder.BadRequest(c, "Empty Email, Pass in valid value")
+	// }
+
+	// user, err := db.GetUser(email)
+	// if err != nil {
+	// 	return responder.InternalServerError(c, "Unable to add user to roster")
+	// }
+
+	// err := db.PostUserToRoster(rosterName, user)
+	// if err != nil {
+	// 	log.WithErr(err).Alert("Failed to add user to roster")
+	// 	return err
+	// }
+
+	roster := createRoster(capt)
+
+	err = db.PostRoster(roster)
+	if err != nil {
+		return responder.InternalServerError(c, "Failed to create roster!")
+	}
+
+	return responder.Ok(c)
 }
 
 func getRosters(c *fiber.Ctx) error {
@@ -23,4 +74,23 @@ func getRosters(c *fiber.Ctx) error {
 	}
 
 	return responder.OkWithData(c, rosters)
+}
+
+// func postRosters(c *fiber.Ctx) error {
+// 	db := db.GetSession(c)
+// 	err := db.PostRosters()
+// 	if err != nil {
+// 		log.WithErr(err).Alert("Failed to post roster to the database")
+// 		return err
+// 	}
+
+// 	return responder.Ok(c)
+// }
+
+func createRoster(capt *models.User) *models.Roster {
+	return &models.Roster{
+		CaptainID: capt.ID,
+		Captain:   *capt,
+		Players:   nil,
+	}
 }
