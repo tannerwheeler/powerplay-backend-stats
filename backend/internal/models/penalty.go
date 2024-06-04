80dfe4421e88788fc0ed84232d16f0a4f8e149a5
OughtNaut <112574273+OughtNaut@users.noreply.github.com>
2024-06-04 16:10:32 -0600

    Working on possible solutions to the bug. Found three possibilities i should discuss with team.(pointers rather than actual values, using sql.NullDataType, and gopkg.in/guregu/null.v3 package which seems promising as it can be unmarshalled easily.)

--- backend/internal/models/penalty.go
+++ backend/internal/models/penalty.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"database/sql"
+)
+
+
 type PenaltyType struct {
 	DbModel
 	Name     string `json:"name"`
@@ -7,14 +12,16 @@ type PenaltyType struct {
 	Severity string `json:"severity"`
 }
 
+// TODO used a pointer rather than uint, this allows the NOT NULL tag to behave as expected
+// Need to see if this is okay with team. Another possible soln is using sql.nullInt
 type Penalty struct {
 	DbModel
 	PlayerID      uint        `json:"player_id"`
 	TeamID        uint        `json:"team_id"`
 	GameID        uint        `json:"game_id"`
 	Period        uint        `json:"period"`
-	Duration      uint        `json:"duration"`
-	CreatedBy     uint        `json:"created_by"`
+	Duration      sql.NullInt64        `json:"duration" gorm:"NOT NULL"`
+	CreatedBy     *uint        `json:"created_by" gorm:"NOT NULL"`
 	PenaltyType   PenaltyType `json:"penalty_type"`
 	PenaltyTypeID uint        `json:"penalty_type_id"`
 }
