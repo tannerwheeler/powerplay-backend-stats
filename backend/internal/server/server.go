e51475a001ad95e837d3c01781d4936081621e96
tannerwheeler <tancelloman@gmail.com>
2024-06-06 19:04:44 -0600

    Created structure of sport/components rather than groups.

--- backend/internal/server/server.go
+++ backend/internal/server/server.go
@@ -12,8 +12,8 @@ import (
 	// Blank imports for apis to cause init functions to run
 	_ "github.com/jak103/powerplay/internal/server/apis/auth"
 	_ "github.com/jak103/powerplay/internal/server/apis/chat"
-	_ "github.com/jak103/powerplay/internal/server/apis/groups"
 	_ "github.com/jak103/powerplay/internal/server/apis/notifications"
+	_ "github.com/jak103/powerplay/internal/server/apis/sports/components"
 	_ "github.com/jak103/powerplay/internal/server/apis/stats"
 	_ "github.com/jak103/powerplay/internal/server/apis/user"
 )
