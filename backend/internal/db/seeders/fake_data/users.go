f8c05f7da4f6233929888039a37fb94b7eca5679
caleb <caleb.r.edwards@gamil.com>
2024-06-10 23:36:09 -0600

    more changes

--- backend/internal/db/seeders/fake_data/users.go
+++ backend/internal/db/seeders/fake_data/users.go
@@ -7,6 +7,7 @@ import (
 	"github.com/go-faker/faker/v4"
 	"github.com/jak103/powerplay/internal/models"
 	"github.com/jak103/powerplay/internal/server/services/auth"
+	"github.com/jak103/powerplay/internal/utils/log"
 	"gorm.io/gorm"
 )
 
@@ -18,17 +19,22 @@ func (s UserSeeder) Seed(db *gorm.DB, args ...interface{}) (interface{}, error) 
 	var createdUsers []models.User
 	for i := 0; i < 4; i++ {
 		randIndex := rand.Intn(len(roles))
+		role := roles[randIndex]
+		log.Debug("Role : %v, Type : %T", role, role)
+
 		user := models.User{
 			FirstName:    faker.FirstName(),
 			LastName:     faker.LastName(),
 			Email:        faker.Email(),
 			Password:     faker.Password(),
 			Phone:        faker.Phonenumber(),
-			Role:         roles[randIndex],
+			Role:         role,
 			SkillLevel:   rand.Intn(5),
 			CurrentTeams: []models.Team{},
 			DateOfBirth:  time.Time{},
 		}
+
+		log.Debug("User : %v, Type : %T", user, user)
 
 		if err := db.FirstOrCreate(&user, models.User{}).Error; err != nil {
 			return nil, err
