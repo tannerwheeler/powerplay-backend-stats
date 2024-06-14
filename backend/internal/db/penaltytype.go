42079d13cd3b51b1859a87446140a390e40992bc
Ryan Barlow <barlow.ryan@rocketmail.com>
2024-06-13 19:37:16 -0600

    One more merge result

--- backend/internal/db/penaltytype.go
+++ backend/internal/db/penaltytype.go
@@ -4,26 +4,26 @@ import "github.com/jak103/powerplay/internal/models"
 
 func (s session) GetPenaltyTypes() ([]models.PenaltyType, error) {
 	penaltyTypes := make([]models.PenaltyType, 0)
-	err := s.connection.Find(&penaltyTypes)
+	err := s.Find(&penaltyTypes)
 	return resultsOrError(penaltyTypes, err)
 }
 
 func (s session) GetPenaltyTypeByID(id string) (*models.PenaltyType, error) {
 	var penaltyType *models.PenaltyType
-	err := s.connection.First(&penaltyType, "id = ?", id)
+	err := s.First(&penaltyType, "id = ?", id)
 	return resultOrError(penaltyType, err)
 }
 
 func (s session) CreatePenaltyType(request *models.PenaltyType) (*models.PenaltyType, error) {
-	err := s.connection.Create(request)
+	err := s.Create(request)
 	return resultOrError(request, err)
 }
 
 func (s session) UpdatePenaltyType(request *models.PenaltyType) (*models.PenaltyType, error) {
-	err := s.connection.Save(request)
+	err := s.Save(request)
 	return resultOrError(request, err)
 }
 
 func (s session) DeletePenaltyType(request *models.PenaltyType) error {
-	return s.connection.Delete(request).Error
+	return s.Delete(request).Error
 }
